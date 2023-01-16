a806b4af3cff2cfe8ce9f9e7660cede67fda792a
Carl Verrier <carlitox@Carls-MBP.attlocal.net>
2023-01-16 16:02:57 -0500

    nav responsive

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -4,13 +4,13 @@ import { NavLink } from "react-router-dom";
 const Nav = () => {
   return (
     <div className="nav-wrp container-fluid row">
-      <div className="col col-sm-8 pb-0">
+      <div className="col col-sm-6 pb-0">
         <NavLink exact={true} to="/">
           <h4>My Story</h4>
         </NavLink>
       </div>
 
-      <div className="col row">
+      <div className="col col-sm-6 row">
         <div className="col text-end">
           <NavLink to="/projects">Projects</NavLink>
         </div>
--- src/Components/ProfileCard.jsx
+++ src/Components/ProfileCard.jsx
@@ -11,7 +11,7 @@ const ProfileCard = () => {
           <img
             width="250"
             height="250"
-            src="https://media-exp1.licdn.com/dms/image/C4E03AQGlPoMGXGbdaQ/profile-displayphoto-shrink_800_800/0/1663698452447?e=1669248000&v=beta&t=Oz6qwWwQdZxMtCt-UlVdxXQJhh9dvzBKfUJz4lHEQEA"
+            src="https://media.licdn.com/dms/image/C4E03AQGlPoMGXGbdaQ/profile-displayphoto-shrink_800_800/0/1663698452447?e=1679529600&v=beta&t=kfn2RW4B28rxYwQ3UdbEG7Jh1UEC4Mae_6ejkVnekfs"
             alt="pic-of-me"
           />
         </div>
